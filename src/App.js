a192a2cafcd3a6a5f4910195c15159807e043886
shakibahadidi <127073032+shakibahadidi@users.noreply.github.com>
2023-05-04 16:47:19 +0330

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This code was created by Shakiba Hadidi and is{" "}
+        <a href="https://github.com/shakibahadidi/my-app" target="_blank">
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
