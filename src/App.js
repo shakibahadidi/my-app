87caf791569e9db9426438df7ad9b408a180cde4
shakibahadidi <127073032+shakibahadidi@users.noreply.github.com>
2023-05-06 10:48:37 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This code was created by Shakiba Hadidi and is{" "}
-        <a href="https://github.com/shakibahadidi/my-app" target="_blank">
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This code was created by Shakiba Hadidi and is{" "}
+          <a href="https://github.com/shakibahadidi/my-app" target="_blank">
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
